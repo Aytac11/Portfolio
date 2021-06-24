8afe483543dc88bc85e754aa4f12f7dfae06db93
Aytac11 <aytac11011@gmail.com>
2021-06-24 15:26:24 +0400

    add skills section

--- js/script.js
+++ js/script.js
@@ -57,3 +57,26 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+// --------------progress----------
+
+var i = 0;
+
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var width = 1;
+        var id = setInterval(frame, 10);
+
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
